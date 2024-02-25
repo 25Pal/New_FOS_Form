8c91926b0f487115a5fb09ba6816341bc7033034
chetanbnagmoti <cbn9860@gmail.com>
2024-02-25 20:45:35 +0530

    Added

--- src/Component/CustumComponet/TextArea.js
+++ src/Component/CustumComponet/TextArea.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 const TextArea = ({labelName,idName,dynamicKey,errors,register,lg,required}) => {
-    
-  
   return (
     <>
       <div className={`col-lg-${lg ? lg:4} col-md-6 col-xs-12 input-wrapper`}>
